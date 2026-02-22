c4a7b481a0a3d417a455ab3ef85031ff65c17c01
keks137 <keks137@proton.me>
2026-02-22 20:53:26 +0100

    make polling example less roundabout

--- example/5-input_polling_based.c
+++ example/5-input_polling_based.c
@@ -3,14 +3,6 @@
 #define LOADOPENGL_IMPLEMENTATION
 #include "../loadopengl.h"
 
-#define key_pressed(k) (!key_prev[k].pressed && key_current[k].pressed)
-#define key_released(k) (key_prev[k].pressed && !key_current[k].pressed)
-#define key_held(k) (key_current[k].pressed)
-#define key_mod_ctl(k) ((key_current[k].pressed & YW_KEYMOD_CTRL))
-#define key_mod_alt(k) ((key_current[k].pressed & YW_KEYMOD_ALT))
-#define key_mod_super(k) ((key_current[k].pressed & YW_KEYMOD_SUPER))
-#define key_mod_shift(k) ((key_current[k].pressed & YW_KEYMOD_SHIFT))
-
 int main()
 {
 	YwState s = { 0 };
@@ -21,21 +13,21 @@ int main()
 	YwSetVSync(&w, true);
 	struct GLFuncs gl = { 0 };
 	load_gl_functions(&s, &gl);
+	bool b_down;
 
 	while (!w.should_close) {
 		YwPollEvents(&w);
 		memcpy(key_prev, key_current, sizeof(key_prev));
 		YwKeyEvent keyev = { 0 };
 		while (YwNextKeyEvent(&w, &keyev)) {
-			key_current[keyev.key] = keyev;
+			if (keyev.key == YW_KEY_B) {
+				b_down = keyev.pressed;
+			}
 		}
 		YwBeginDrawing(&w);
 		gl.ClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-		if (key_held(YW_KEY_B)) {
+		if (b_down) {
 			gl.ClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-		}
-		if (key_held(YW_KEY_B) && key_mod_ctl(YW_KEY_B)) {
-			gl.ClearColor(1.0f, 0.0f, 1.0f, 1.0f);
 		}
 		gl.Clear(GL_COLOR_BUFFER_BIT);
 		YwEndDrawing(&w);
